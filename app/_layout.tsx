2ee680d74fb55f1ffc7ebc2cc8264d64a6ca1aa3
Dalawer Hossain Juwel <juwelrana090@gmail.com>
2025-06-24 13:51:19 +0600

    Setup React Native with TailwinCss

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,12 @@
 import { Stack } from "expo-router";
+import { StatusBar } from "react-native";
 import "./globals.css";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <>
+      <StatusBar hidden={false} />
+      <Stack />
+    </>
+  );
 }
